ad71865b7c6d9ddb61f75c57b04af898778273e9
Luiz Baldi <baldilp@gmail.com>
2017-01-26 22:55:20 -0200

    changing project structure and solving small bugs

---
+++ backend/partner.php
@@ -0,0 +1,10 @@
+<?php
+	$owner = $_POST['owner'];
+	$company = $_POST['company'];
+	$email = $_POST['email'];
+	$city = $_POST['city'];
+	$state = $_POST['owner'];
+
+	// Do whatever what you want with the data, dear Phil.
+	// Ps: it's already been validated and shit.
+?>
---
+++ frontend/assets/js/register.js
@@ -0,0 +1,46 @@
+function registerPartner() {
+	var registerData = {};
+
+	if (_validateForm()) {
+		registerData.owner = document.forms[0].owner.value;
+		registerData.company = document.forms[0].company.value;
+		registerData.email = document.forms[0].email.value;
+		registerData.city = document.forms[0].city.value;
+		registerData.state = document.forms[0].owner.value;
+
+		$.ajax({
+	    	type: "POST",
+		    url: '../backend/partner.php',
+		    data: registerData,
+		    success: function(data){
+		        alert('Obrigado por entrar em contato! Em breve enviaremos um email de retorno :)');
+		        window.location = 'index.html';
+		    }
+		});
+	}
+};
+
+var _validateForm = function() {
+	if (isEmpty(document.forms[0].owner.value)) {
+		alert('Preencha o nome');
+		return false;
+	} else if (isEmpty(document.forms[0].company.value)) {
+		alert('Preencha a empresa');
+		return false;
+	} else if (isEmpty(document.forms[0].email.value)) {
+		alert('Preencha o email');
+		return false;
+	} else if (isEmpty(document.forms[0].city.value)) {
+		alert('Preencha a cidade');
+		return false;
+	} else if (isEmpty(document.forms[0].state.value)) {
+		alert('Preencha o estado');
+		return false;
+	}
+
+	return true;
+};
+
+function isEmpty(value) {
+    return (value === undefined || value == null || value.length <= 0 || value == "") ? true : false;
+}
